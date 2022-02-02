5f7a076b4189a9fe92ed427c77069d721fc09be3
Spacekitsune <70965432+Spacekitsune@users.noreply.github.com>
2022-02-02 21:07:48 +0200

    21:07

--- resources/views/articleimage/show.blade.php
+++ resources/views/articleimage/show.blade.php
@@ -2,20 +2,28 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <h1>{{$articleCategory->title}}</h1>
+<div class="album py-5 bg-light">
+    <div class="container">
+        <div class="row row-cols-1 g-3">
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="{{'/images/'.$articleImage->src}}" class="bd-placeholder-img card-img-top" alt="{{$articleImage->alt}}" width="{{$articleImage->height}}" height="{{$articleImage->height}}">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a class="btn btn-primary" href="{{route('articleimage.show', [$articleImage])}}">Show</a>
+                                <a class="btn btn-success" href="{{route('articleimage.edit', [$articleImage])}}">Edit</a>
 
-                <p>{{$articleCategory->description}}</p>
-
-
-
+                                <form action="{{route('articleimage.destroy', [$articleImage])}}" method="POST">
+                                    @csrf
+                                    <button class="btn btn-danger" type="submit">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="container my-6">
-                <a href="{{ route('articlecategory.index') }}" class="btn btn-info">Back</a>
-            </div>
+
         </div>
     </div>
 </div>
